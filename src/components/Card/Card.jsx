921380ab3a27acb6dacde95ec6d8e9351cd16643
JahanaSultan <cahana.aliyeva@gmail.com>
2023-08-18 13:55:11 +0400

    update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,32 +4,36 @@ import basket from "../../assets/icons/shopping-basket-line.svg";
 import heart from "../../assets/icons/heart-line.svg";
 import eye from "../../assets/icons/eye-line.svg";
 import SaleRibbon from "../SaleRibbon/SaleRibbon";
-import "./Card.css"
+import "./Card.css";
+import ReactStars from "react-rating-stars-component";
 
 const Card = () => {
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
   return (
     <div className="card d-flex flex-column">
-      <SaleRibbon/>
+      <SaleRibbon />
       <div className="card-image">
-        <ul className="card-side-nav flex-center">
-          <li className="flex-center">
-            <img src={basket} alt="basket icon" />
-          </li>
-          <li className="flex-center">
-            <img src={heart} alt="basket icon" />
-          </li>
-          <li className="flex-center">
-            <img src={eye} alt="basket icon" />
-          </li>
-        </ul>
         <img src={tea} alt="product" />
       </div>
-      <Link to="#" className="card-bottom d-flex p1">
-        <div className="left">
-          <h4>Masala tea</h4>
-          <p className="category">Çaylar</p>
+      <Link to="#" className="card-bottom d-flex flex-column">
+        <div className="stars flex-center">
+          <ReactStars
+            count={5}
+            onChange={ratingChanged}
+            size={24}
+            activeColor="#ffd700"
+          />
         </div>
-        <div className="right flex-center">12 ₼</div>
+        <div className="content d-flex p1">
+          <div className="left ">
+            <h4>Masala tea</h4>
+            <small className="category">Çaylar</small>
+          </div>
+          <div className="right flex-center">12 ₼</div>
+        </div>
+        <button className="add-to-cart">Səbətə Əlavə Et</button>
       </Link>
     </div>
   );
