7c986ada17effbe1eda6164612d6d09dbde32a26
Jahana <cahana.aliyeva@gmail.com>
2024-01-25 23:38:31 +0400

    Add aos library and update styles

--- src/components/Categories1/Categories.jsx
+++ src/components/Categories1/Categories.jsx
@@ -4,16 +4,23 @@ import teacup from "../../assets/img/tea1.jpg";
 import honey from "../../assets/img/choney.jpg";
 import coffee from "../../assets/img/coffee1.jpg";
 import teaware from "../../assets/img/teaware.jpg";
+import tea from "../../assets/img/teabg.png"
 
 
 const Categories = () => {
   return (
-      <section className="categories d-flex">
-        <CategoryCard text="Çaylar" img={teacup} />
-        <CategoryCard text="Qəhvələr" img={coffee} />
-        <CategoryCard text="Ballar" img={honey} />
-        <CategoryCard text="Ləvazimatlar" img={teaware} />
-      </section>
+      // <section className="categories d-flex">
+      //   <CategoryCard text="Çaylar" img={teacup} />
+      //   <CategoryCard text="Qəhvələr" img={coffee} />
+      //   <CategoryCard text="Ballar" img={honey} />
+      //   <CategoryCard text="Ləvazimatlar" img={teaware} />
+      // </section>
+
+      <header>
+        <div className="path">
+          <img src={tea} alt="" />
+        </div>
+      </header>
 
   );
 };
